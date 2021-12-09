6ffe0922745903753ec98ca778f9485583e0e2d9
Gustavo Ellwanger <ellwangerinvestimentos@gmail.com>
2021-12-09 15:04:58 -0300

    Adiciona requisito 1 ao projeto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './components/Routes';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes!!</p>);
+    return (
+      <BrowserRouter>
+        <h1>TrybeTunes</h1>
+        <div>
+          <Routes />
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 
