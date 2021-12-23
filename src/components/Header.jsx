207a3c0b79eef6ba2fe0951ce671531c2af0d5c1
Gustavo Ellwanger <ellwangerinvestimentos@gmail.com>
2021-12-22 21:07:04 -0300

    Adiciona requisito 4 8/17

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -33,8 +34,13 @@ class Header extends Component {
             <h2 data-testid="header-user-name">{nameInput}</h2>
           )}
         </header>
-        <nav data-testid="link-to-search">
-          teste
+        <nav className="links">
+          <Link to="/search" data-testid="link-to-search">
+            Search
+          </Link>
+          <Link to="/favorites" data-testid="link-to-favorites">
+            Favorite Songs
+          </Link>
         </nav>
       </main>
     );
