d30d3099a4ba9cf41977784368a3f2694069cb32
Gustavo Ellwanger <ellwangerinvestimentos@gmail.com>
2021-12-24 11:53:59 -0300

    Adiciona 1/5 do requisito 6

---
+++ src/components/ShowSearch.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ShowSearch extends Component {
+  render() {
+    return (
+      <h1>Show Search</h1>
+    );
+  }
+}
+
+export default ShowSearch;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,14 +1,91 @@
 import React, { Component } from 'react';
-// import searchAlbumAPI from '../services/searchAlbumsAPI';
+import searchAlbumAPI from '../services/searchAlbumsAPI';
+
+import Loading from '../components/Loading';
 
 class Search extends Component {
-  searchAlbum = () => {
-    console.log('testeSearch');
+  constructor() {
+    super();
+
+    this.state = {
+      searchInAPI: '',
+      loading: false,
+      buttonDisable: true,
+    };
+  }
+
+  handleSubmit = ({ target }) => {
+    const { name, value } = target;
+    const inputName = value;
+    this.setState({ [name]: inputName }, this.enableButton);
+  }
+
+  callSearchAlbum = () => {
+    const { searchInAPI } = this.state;
+
+    searchAlbumAPI(searchInAPI)
+      .then((dataAlbums) => {
+        this.setState({
+          loading: false,
+          // answerOfSearch: searchInAPI,
+          allAlbums: [...dataAlbums],
+        });
+      });
+  }
+
+  enableButton = () => {
+    const { searchInAPI } = this.state;
+    const minimumLength = 2;
+
+    if (searchInAPI.length >= minimumLength) this.setState({ buttonDisable: false });
+  }
+
+  searchButton = () => {
+    this.callSearchAlbum();
+    this.setState({
+      searchInAPI: '',
+      loading: true,
+      buttonDisable: true,
+    });
   }
 
   render() {
+    const { loading, searchInAPI, buttonDisable } = this.state;
+
+    const searchInput = (
+      <input
+        value={ searchInAPI }
+        onChange={ this.handleSubmit }
+        data-testid="search-artist-input"
+        type="text"
+        name="searchInAPI"
+        id="searchInAPI"
+        placeholder="Faça sua busca aqui"
+      />
+    );
+
+    const searchArtistOrBand = (
+      <form>
+        <label htmlFor="searchInAPI">
+          Digite aqui seu Artista ou Banda:
+          { searchInput }
+        </label>
+      </form>
+    );
+
     return (
-      <h1>teste</h1>
+      <div className="searchPage">
+        <h1>Search</h1>
+        { loading ? <Loading /> : searchArtistOrBand}
+        <button
+          data-testid="search-artist-button"
+          type="button"
+          disabled={ buttonDisable }
+          onClick={ this.searchButton }
+        >
+          Search
+        </button>
+      </div>
     );
   }
 }
